65f73827ff52c9203d3f128e4d1c7c8832008618
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-01 14:03:29 +0300

    Initial Commit

---
+++ CupcakeCorner/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CupcakeCorner
+//
+//  Created by Omer Cagri Sayir on 1.01.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Omer Cagri Sayir on 1.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
