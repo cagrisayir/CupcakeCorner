a65c27716f1505eb403f944db8804c7df4ba420e
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-07 19:32:27 +0300

    taking basic orders

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -9,50 +9,38 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var results = [Result]()
+    @State private var order = Order()
 
     var body: some View {
-        VStack {
-            AsyncImage(url: URL(string: "https://hws.dev/img/logo.png")) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-            } placeholder: {
-                ProgressView()
-            }
-            .frame(width: 200, height: 200)
-            
-            List(results, id: \.trackId) { item in
-                VStack(alignment: .leading) {
-                    Text(item.trackName)
-                        .font(.headline)
-                    Text(item.collectionName)
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Select your cake type", selection: $order.type) {
+                        ForEach(Order.types.indices, id: \.self) {
+                            Text(Order.types[$0])
+                        }
+                    }
+
+                    Stepper("Number of cakes: \(order.quantity)", value: $order.quantity, in: 3 ... 20)
+                }
+
+                Section {
+                    Toggle("Any special requests?", isOn: $order.specialRequestEnabled)
+
+                    if order.specialRequestEnabled {
+                        Toggle("Add extra frosting", isOn: $order.extraFrosting)
+
+                        Toggle("Add extra sprinkles", isOn: $order.addSprinkles)
+                    }
+                }
+                Section {
+                    NavigationLink("Delivery details") {
+                        AddressView(order: order)
+                    }
                 }
             }
-            .task {
-                await loadData()
+            .navigationTitle("Cupcake Corner")
         }
-        }
-    }
-
-    func loadData() async {
-        // 1. Creating the URL we want to read
-        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
-            print("Invalid URL")
-            return
-        }
-        // 2. Fetching the data for that URL
-        do {
-            let (data, _) = try await URLSession.shared.data(from: url)
-            
-            // 3. Decoding the result of that data into a Response struct
-            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
-                results = decodedResponse.results
-            }
-        } catch {
-            print("Invalid data")
-        }
-        
-        
     }
 }
 
