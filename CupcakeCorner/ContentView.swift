5962df618ff5aef8971e736b47bcae5c602f1a7f
Omer Cagri Sayir <omercagris@gmail.com>
2024-01-07 18:27:43 +0300

    receivied data with URLSession

--- CupcakeCorner/ContentView.swift
+++ CupcakeCorner/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var results = [Result]()
+
     var body: some View {
         List(results, id: \.trackId) { item in
             VStack(alignment: .leading) {
@@ -17,6 +18,30 @@ struct ContentView: View {
                 Text(item.collectionName)
             }
         }
+        .task {
+            await loadData()
+        }
+    }
+
+    func loadData() async {
+        // 1. Creating the URL we want to read
+        guard let url = URL(string: "https://itunes.apple.com/search?term=taylor+swift&entity=song") else {
+            print("Invalid URL")
+            return
+        }
+        // 2. Fetching the data for that URL
+        do {
+            let (data, _) = try await URLSession.shared.data(from: url)
+            
+            // 3. Decoding the result of that data into a Response struct
+            if let decodedResponse = try? JSONDecoder().decode(Response.self, from: data) {
+                results = decodedResponse.results
+            }
+        } catch {
+            print("Invalid data")
+        }
+        
+        
     }
 }
 
